39f0f8c9ce359bf8aa27307ccfc8bf305167ea0c
LPerez <alperezp@puce.edu.ec>
2025-01-11 10:38:15 -0500

    Parcial02

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=30, null=False)
+    gender = models.CharField(max_length=30, null=False)
+    director = models.CharField(max_length=30, null=False)
+    release_year = models.CharField(max_length=30, null=False)
+    synopsis = models.CharField(max_length=400, null=False)
+    
+    def __str__(self):
+        return f'{self.title} - {self.gender}'
+
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
+from .models import Movie
 # Create your views here.
+
+def index(request):
+    movie = Movie.objects.order_by('title')
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({
+        'movies': movie,
+    },
+    request))
